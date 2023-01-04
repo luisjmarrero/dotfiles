7ed05a671203e42104cc13d6b79b3b3909c39cf6
Luis Marrero <luisjmarrerop@gmail.com>
2023-01-04 15:57:21 -0400

    add nvm to rc file

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -41,6 +41,10 @@ if uname -r |grep -q 'WSL2' ; then
     [ ! -f /home/linuxbrew/.linuxbrew/bin/brew ] || eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
