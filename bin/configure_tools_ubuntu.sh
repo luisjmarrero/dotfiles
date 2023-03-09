9bc737ec81162d98abbe444dbe01dfcd927aafd9
Luis Marrero <luisjmarrerop@gmail.com>
2023-03-09 13:51:00 -0400

    install script

--- bin/configure_tools_ubuntu.sh
+++ bin/configure_tools_ubuntu.sh
@@ -10,3 +10,12 @@ ln -sf ~/.dotfiles/zsh/alias.zsh ~/.zsh/alias.zsh
 mkdir -p ~/.config
 mkdir -p ~/.config/alacritty
 ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# alacritty
+ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.ym
+
+# tmux
+ln -sf ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+# nvim
+ln -sf ~/.dotfiles/nvim/* ~/.config/nvim
