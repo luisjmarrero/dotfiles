3402f872f2b01dcfa4fa98439855bf0408cd3867
Luis Marrero <luisjmarrerop@gmail.com>
2023-09-20 16:23:53 -0400

    update dotfiles

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,3 +3,7 @@ export PATH=$PATH:/usr/local/go/bin
 
 # add packages installed with go to the path
 export PATH=$(go env GOPATH)/bin:$PATH
+
+export JAVA7_HOME=~/.sdkman/candidates/java/7.0.352-zulu
+
+export PATH="$PATH:/home/luis.marrero/.local/bin"
