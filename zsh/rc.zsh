fc41fa02b417cdc2d77b46358e3c5e35b8963482
Luis Marrero <luisjmarrerop@gmail.com>
2025-05-19 12:56:57 -0400

    redo my dotfiles

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -52,5 +52,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# DO NOT COMMIT ZONE
-source ~/.globo_profile
+[ ! -f  ~/.globo_profile ] || source ~/.globo_profile
